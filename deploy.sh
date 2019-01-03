3fce6f4439d995b947f3e269f57bf2e165cc2dbc
Imdad Ahmed <imdhmd@gmail.com>
2019-01-03 15:11:50 +0000

    Initial commit for pipeline

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -xe
+
+echo "Deploying pet-db..."
+kubectl apply -f kubernetes/mysql.yml --namespace default
+
+IMAGE_VERSION=${GO_PIPELINE_LABEL:-latest}
+PROJECT_ID=${GCLOUD_PROJECT_ID:-devops-workshop-03012019}
+echo "Deploying pet-web image version: $IMAGE_VERSION"
+
+cat kubernetes/web.yml | sed "s/\(image: \).*$/\1us.gcr.io\/$PROJECT_ID\/pet-app:$IMAGE_VERSION/" | kubectl apply -f - --namespace default
